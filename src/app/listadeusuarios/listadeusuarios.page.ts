ce161355ffa025c40b5e6dbf0f3c2578683226f6
Araceli RL <1802075@utrivieramaya.edu.mx>
2022-03-22 20:19:22 -0500

    Initial commit

---
+++ src/app/listadeusuarios/listadeusuarios.page.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
+
+@Component({
+  selector: 'app-listadeusuarios',
+  templateUrl: './listadeusuarios.page.html',
+  styleUrls: ['./listadeusuarios.page.scss'],
+})
+export class ListadeusuariosPage implements OnInit {
+
+  constructor(private router: Router) {
+  }
+
+  ngOnInit() {
+  }
+
+  gotohome() {
+    this.router.navigate(['/tabs/home']);
+  }
+  gotochat() {
+    this.router.navigate(['/tabs/chat']);
+  }
+
+
+}
+
